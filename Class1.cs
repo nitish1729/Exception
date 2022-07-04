0ac62107ef5a4c6c0981c1f9a7334bb513e5a809
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-05 03:00:48 +0530

    Exception

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Notes
+{
+    public class Class1
+    {
+        public static void show()
+        { 
+            string welcome = "Exception";
+            Console.WriteLine(welcome);
+        }
+        public static void Main()
+        {
+            show();
+        } 
+    }
+}
